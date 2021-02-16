3ee28ab94ae05e310b153e98b800c0516275533a
Kate1199 <kate.kisel99@yandex.by>
2021-02-16 18:33:46 +0300

    0

--- src/academy/belhard/Main.java
+++ src/academy/belhard/Main.java
@@ -1,8 +1,42 @@
 package academy.belhard;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Address a1 = new Address("Минск", "Независимости", 45);
+        Address a2 = new Address("Минск", "Я.Коласа", 18);
+        Address a3 = new Address("Брест", "Советская", 24);
+
+        Person p1 = new Person("Василий", "Калинин", a1);
+        Person p2 = new Person("Анна", "Зубова", a1);
+        Person p3 = new Person("Мария", "Виноградова", a2);
+        Person p4 = new Person(null, "Симонов", a2);
+        Person p5 = new Person("Михаил", null, a3);
+        Person p6 = new Person("Даниил", "Беляев", null);
+
+        List<Person> people = new ArrayList<>();
+        people.add(p1);
+        people.add(p2);
+        people.add(p3);
+        people.add(p4);
+        people.add(p5);
+        people.add(p6);
+
+      List<Person> result = people.stream()
+               .filter(x -> !(x.getFirstName().equals(null)) || !(x.getLastName().equals(null)) || !(x.getAddress().equals(null)))
+               .sorted()
+               .collect(Collectors.toList());
+
+       for(Person p : result) {
+           System.out.println(p.toString());
+       }
     }
 }
---
+++ src/academy/belhard/Person.java
@@ -0,0 +1,36 @@
+package academy.belhard;
+
+public class Person implements Comparable<Person>{
+
+    private String firstName;
+    private String lastName;
+    private Address address;
+
+    public Person(String firstName, String lastName, Address address) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    @Override
+    public String toString() {
+        return firstName + " " + lastName + ". Адрес: " + address.getCity() + ", улица " +
+                address.getStreet() + " " + address.getHouseNumber();
+    }
+
+    public int compareTo(Person p) {
+        return address.getHouseNumber() - (p.address).getHouseNumber();
+    }
+}
